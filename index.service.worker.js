532c8911cf8a0e394ecc9a900cc3bf5847aaff76
emiepic <emiiepic02@gmail.com>
2024-01-12 23:04:30 +0000

    Fix looping intro noise

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1705096956|1181592462";
+const CACHE_VERSION = "1705100652|4877950043";
 const CACHE_PREFIX = "Delta Experiment-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
