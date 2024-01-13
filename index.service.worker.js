4262bcc977560f14b35a5e251beee319c2d853c4
emiepic <emiiepic02@gmail.com>
2024-01-13 23:39:27 +0000

    Update dialogue

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1705100652|4877950043";
+const CACHE_VERSION = "1705189099|35395203";
 const CACHE_PREFIX = "Delta Experiment-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
